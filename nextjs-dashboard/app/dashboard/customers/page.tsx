baa31656e193635a72c19318e7aebbf30a7192e6
rohanbaral16 <rhnbrl16@gmail.com>
2026-01-08 08:36:36 +0545

    repo initialization

---
+++ nextjs-dashboard/app/dashboard/customers/page.tsx
@@ -0,0 +1,7 @@
+export default function Page(){
+    return(
+        <div className="bg-blue-400 w-screen h-10">
+            <p className="bg-red-300 text-center py-2 border-2 border-red-950">Customer page</p>
+        </div>
+    )
+}
